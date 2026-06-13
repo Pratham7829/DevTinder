49bb05e7b608afe404d8d50fb39d0c449017245b
Pratham Singla <prathamsingla004@gmail.com>
2026-06-13 16:36:08 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express(); // Create an instance of the Express application
+
+app.use("/", (req, res) => {
+    res.send("Hello from the dashboard");
+});
+
+app.use("/hello", (req, res) => {
+    res.send("hello hello hello");
+});
+
+app.use("/test", (req, res) => {
+    res.send("Hello from the server");
+});
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000");
+});

794aed6359b83c5df40e95c2858ec76ae731311f
Pratham Singla <prathamsingla004@gmail.com>
2026-06-14 16:04:27 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -3,17 +3,21 @@ const { connectDB } = require('./config/database');
 const {User} = require('./models/user');
 const app = express(); // Create an instance of the Express application
 
+app.use(express.json()); // Middleware to parse JSON request bodies
+
 app.post("/signup", async (req, res) => {
 
+    // console.log(req.body);
+
     // creating an new instance of the User model
-    const user = new User({
-        firstName: "Pratham",
-        lastName: "Singla",
-        emailId: "pratham.singla@example.com",
-        password: "password123",
-    });
-    await user.save();
-    res.send("user Added successfully");
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("user Added successfully");
+    } catch(err) {
+        console.error("Error adding user:", err);
+        res.status(400).send("Error saving the user" + err.message);
+    }
 });
 
 // we should do app.listen after we habe connected to the database

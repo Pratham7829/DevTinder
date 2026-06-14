b288088f9f36e78cfa5b5df44dad42b6a175a2af
Pratham Singla <prathamsingla004@gmail.com>
2026-06-14 10:58:37 +0530

    exploring route handlers

--- src/app.js
+++ src/app.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const app = express(); // Create an instance of the Express application
 
-app.get("/user", (req, res) => {
-    res.send({firstName: "pratham", lastName: "singla"});
-});
-
-app.post("/user", (req, res) => {
-    res.send("Data has been posted to the server");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Data has been deleted from the server");
-});
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server");
+app.use("/user", [(req, res, next) => {
+    console.log("First route handler");
+    next();
+    // res.send("Hello from the first route handler!");
+}, (req, res, next) => {
+    console.log("Second route handler");
+    // res.send("Hello from the /user route!");
+    next();
+}], (req, res, next) => {
+    console.log("Third route handler");
+    res.send("Hello from the third route handler!");
 });
 
 app.listen(3000, () => {

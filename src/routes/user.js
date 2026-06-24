d0fed842a43c17fbd6cde874504d855934a223d3
Pratham Singla <prathamsingla004@gmail.com>
2026-06-24 19:16:17 +0530

    user/connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -21,4 +21,34 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }
 });
 
+const USER_SAFE_DATA = "firstName lastName age about photoUrl skills gender";
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id, status: "accepted"},
+                {fromUserId: loggedInUser._id, status: "accepted"}
+            ]
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.equals(loggedInUser._id)) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({
+            message: "Connections found successfully!!!",
+            data
+        });
+    } catch(err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
 module.exports = {userRouter};
